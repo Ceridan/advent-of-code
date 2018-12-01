5a13328c71531c4531c38e12e772426225889f8f
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2018-12-01 19:10:43 +0300

    day01 add tests

--- 2018/day01.py
+++ 2018/day01.py
@@ -12,7 +12,19 @@ def part2(data):
                 return frequency
             known_frequency[frequency] = True
 
+## Tests
+assert part1(['+1', '-2', '+3', '+1']) == 3
+assert part1(['+1', '+1', '+1']) == 3
+assert part1(['+1', '+1', '-2']) == 0
+assert part1(['-1', '-2', '-3']) == -6
 
+assert part2(['+1', '-2', '+3', '+1']) == 2
+assert part2(['+1', '-1']) == 0
+assert part2(['+3', '+3', '+4', '-2', '-4']) == 10
+assert part2(['-6', '+3', '+8', '+5', '-6']) == 5
+assert part2(['+7', '+7', '-2', '-7', '-4']) == 14
+
+## Solve real puzzle 
 filename = 'data/day01.txt'
 content = [line.rstrip('\n') for line in open(filename, 'r')]
 
