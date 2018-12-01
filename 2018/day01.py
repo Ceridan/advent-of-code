89ba319dc06e4245e60d024f22594e0c6ea6e5a6
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2018-12-01 15:01:43 +0300

    Day01 rename task to part

--- 2018/day01.py
+++ 2018/day01.py
@@ -1,8 +1,8 @@
-def task01_1(data):
+def part1(data):
     frequency = sum(int(x) for x in data)
     return frequency
 
-def task01_2(data):
+def part2(data):
     known_frequency = { 0: True }
     frequency = 0
     while True:
@@ -16,5 +16,5 @@ def task01_2(data):
 filename = 'data/day01.txt'
 content = [line.rstrip('\n') for line in open(filename, 'r')]
 
-print('Day 01, task 1: %r' % (task01_1(content)))
-print('Day 01, task 2: %r' % (task01_2(content)))
+print('Day 01, part 1: %r' % (part1(content)))
+print('Day 01, part 2: %r' % (part2(content)))
