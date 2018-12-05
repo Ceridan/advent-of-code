3fd579b28a9aa6d5abe7556d3f05f9b60ef6f7af
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2018-12-05 17:54:00 +0300

    Add improved solution for day05

--- 2018/day05.py
+++ 2018/day05.py
@@ -1,7 +1,31 @@
+import string
 import sys
 
 # Solution
 def part1(data):
+    return process_sequence(data)
+
+def part2(data):
+    current_len = sys.maxsize
+    for c in string.ascii_lowercase:
+        tmp_len = process_sequence(data, c)
+        if (tmp_len < current_len):
+            current_len = tmp_len
+    return current_len
+
+def process_sequence(data, excluded_char = False):
+    stack = []
+    for c in data:
+        if (excluded_char and excluded_char == c.lower()):
+            continue
+        if (len(stack) > 0 and stack[-1] != c and stack[-1].lower() == c.lower()):
+            stack.pop()
+        else:
+            stack.append(c)
+    return len(stack)
+
+# Naive solution
+def part1_naive(data):
     current_len = sys.maxsize
     while current_len > len(data):
         current_len = len(data)
@@ -12,7 +36,7 @@ def part1(data):
             data = data.replace(c2 + c1, '')
     return current_len    
 
-def part2(data):   
+def part2_naive(data):   
     current_len = sys.maxsize
     for i in range(26):
         tmp = data
