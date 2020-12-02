29b1b56fe315e409a40307822e6267519df89b8d
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2020-12-02 12:56:15 +0300

    day02: Solution for parts 1,2

--- day_template.py
+++ day_template.py
@@ -2,11 +2,14 @@ import os
 
 
 # Solution
-def part1(input_data) -> None:
+from typing import List
+
+
+def part1(data: List[str]) -> None:
     pass
 
 
-def part2(input_data) -> None:
+def part2(data: List[str]) -> None:
     pass
 
 
@@ -17,9 +20,9 @@ def test(expected, actual):
 
 # Solve real puzzle
 dir_path = os.path.dirname(__file__)
-file_path = os.path.join(dir_path, 'data/day{{XX}}.txt')
+file_path = os.path.join(dir_path, 'data/dayXX.txt')
 with open(file_path, 'r') as f:
-    input_data = [int(line) for line in f.readlines()]
+    input_data = [line for line in f.readlines()]
 
     print('Day 01, part 1: %r' % (part1(input_data)))
     print('Day 01, part 2: %r' % (part2(input_data)))
