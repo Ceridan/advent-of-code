24f32ed9d83fc52470391fd4cebec2b86f397800
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2020-12-22 11:55:25 +0300

    Day 22: Naive solution for part 2. Too long.

--- 2020/day22.py
+++ 2020/day22.py
@@ -11,15 +11,54 @@ def part1(deck: List[str]) -> int:
         _, num1 = heapq.heappop(player1)
         _, num2 = heapq.heappop(player2)
 
-        if num1 > num2:
-            w = heapq.nlargest(1, player1)[0][0] if player1 else 0
-            heapq.heappush(player1, (w + 1, num1))
-            heapq.heappush(player1, (w + 2, num2))
-        else:
-            w = heapq.nlargest(1, player2)[0][0] if player2 else 0
-            heapq.heappush(player2, (w + 1, num2))
-            heapq.heappush(player2, (w + 2, num1))
+        winner = player1 if num1 > num2 else player2
+        w = heapq.nlargest(1, winner)[0][0] if winner else 0
+        heapq.heappush(winner, (w + 1, max(num1, num2)))
+        heapq.heappush(winner, (w + 2, min(num1, num2)))
 
+    return _calculate_score(player1, player2)
+
+
+def part2(deck: List[str]) -> int:
+    player1, player2 = _parse_deck(deck)
+    _, p1, p2 = _recursive_combat(player1, player2)
+    return _calculate_score(player1, player2)
+
+
+def _recursive_combat(player1: List[Tuple[int, int]], player2: List[Tuple[int, int]]) -> Tuple[bool, List[Tuple[int, int]], List[Tuple[int, int]]]:
+    states = set()
+
+    while player1 and player2:
+        current_state = f'{str([num for _, num in player1])}{str([num for _, num in player2])}'
+        if current_state in states:
+            return True, [], []
+
+        states.add(current_state)
+
+        _, num1 = heapq.heappop(player1)
+        _, num2 = heapq.heappop(player2)
+
+        if num1 <= len(player1) and num2 <= len(player2):
+            new_player1 = [pair for pair in heapq.nsmallest(num1, player1)]
+            heapq.heapify(player1)
+            new_player2 = [pair for pair in heapq.nsmallest(num2, player2)]
+            heapq.heapify(player2)
+            is_p1, _, _ = _recursive_combat(new_player1, new_player2)
+            winner = player1 if is_p1 else player2
+            w = heapq.nlargest(1, winner)[0][0] if winner else 0
+            heapq.heappush(winner, (w + 1, num1 if is_p1 else num2))
+            heapq.heappush(winner, (w + 2, num2 if is_p1 else num1))
+            continue
+
+        winner = player1 if num1 > num2 else player2
+        w = heapq.nlargest(1, winner)[0][0] if winner else 0
+        heapq.heappush(winner, (w + 1, max(num1, num2)))
+        heapq.heappush(winner, (w + 2, min(num1, num2)))
+
+    return len(player1) > 0, player1, player2
+
+
+def _calculate_score(player1: List[Tuple[int, int]], player2: List[Tuple[int, int]]) -> int:
     winner = player1 if player1 else player2
     score = 0
     n = len(winner)
@@ -31,10 +70,6 @@ def part1(deck: List[str]) -> int:
         i += 1
 
     return score
-
-
-def part2(deck: List[str]) -> None:
-    pass
 
 
 def _parse_deck(deck: List[str]) -> Tuple[List[Tuple[int, int]], List[Tuple[int, int]]]:
@@ -87,6 +122,21 @@ test(306, part1([
     '10',
 ]))
 
+test(291, part2([
+    'Player 1:',
+    '9',
+    '2',
+    '6',
+    '3',
+    '1',
+    '',
+    'Player 2:',
+    '5',
+    '8',
+    '4',
+    '7',
+    '10',
+]))
 
 file_path = os.path.join(os.path.dirname(__file__), 'data/day22.txt')
 with open(file_path, 'r') as f:
