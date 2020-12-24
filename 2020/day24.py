8fc27064d4e11deac10a27655efcfbfee6560bc2
Mikhail Kumachev <mikhail.kumachev@gmail.com>
2020-12-24 09:29:39 +0300

    Day 24: Solution for part 2

--- 2020/day24.py
+++ 2020/day24.py
@@ -1,6 +1,7 @@
 import os
+from collections import defaultdict
 
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Set
 
 # Coordinates image: http://devmag.org.za/blog/wp-content/uploads/2013/08/screen_136.png
 DIRECTIONS_TO_COORDS = {
@@ -30,7 +31,36 @@ def part1(tiles_list: List[str]) -> int:
 
 
 def part2(tiles_list: List[str], days: int) -> int:
-    pass
+    tiles = _parse_tiles_list(tiles_list)
+    black_tiles = set([coords for coords, tile in tiles.items() if tile.color])
+
+    for day in range(1, days + 1):
+        black_tiles = _generate_next_state(black_tiles)
+
+    return len(black_tiles)
+
+
+def _generate_next_state(black_tiles: Set[Tuple[int, int]]) -> Set[Tuple[int, int]]:
+    new_black_tiles = set()
+    adjacent_white_tiles = defaultdict(int)
+
+    for x, y in black_tiles:
+        adjacent = 0
+        for v in DIRECTIONS_TO_COORDS.values():
+            coord = (x + v[0], y + v[1])
+            if coord in black_tiles:
+                adjacent += 1
+            else:
+                adjacent_white_tiles[coord] += 1
+
+        if adjacent == 1 or adjacent == 2:
+            new_black_tiles.add((x, y))
+
+    for coord, amount in adjacent_white_tiles.items():
+        if amount == 2:
+            new_black_tiles.add(coord)
+
+    return new_black_tiles
 
 
 def _parse_tiles_list(tiles_list: List[str]) -> Dict[Tuple[int, int], Tile]:
@@ -61,7 +91,7 @@ def test(expected, actual):
     assert expected == actual, f'Expected: {expected}, Actual: {actual}'
 
 
-test(10, part1([
+test_input_data = [
     'sesenwnenenewseeswwswswwnenewsewsw',
     'neeenesenwnwwswnenewnwwsewnenwseswesw',
     'seswneswswsenwwnwse',
@@ -82,30 +112,10 @@ test(10, part1([
     'eneswnwswnwsenenwnwnwwseeswneewsenese',
     'neswnwewnwnwseenwseesewsenwsweewe',
     'wseweeenwnesenwwwswnew',
-]))
+]
 
-# test(2208, part2([
-#     'sesenwnenenewseeswwswswwnenewsewsw',
-#     'neeenesenwnwwswnenewnwwsewnenwseswesw',
-#     'seswneswswsenwwnwse',
-#     'nwnwneseeswswnenewneswwnewseswneseene',
-#     'swweswneswnenwsewnwneneseenw',
-#     'eesenwseswswnenwswnwnwsewwnwsene',
-#     'sewnenenenesenwsewnenwwwse',
-#     'wenwwweseeeweswwwnwwe',
-#     'wsweesenenewnwwnwsenewsenwwsesesenwne',
-#     'neeswseenwwswnwswswnw',
-#     'nenwswwsewswnenenewsenwsenwnesesenew',
-#     'enewnwewneswsewnwswenweswnenwsenwsw',
-#     'sweneswneswneneenwnewenewwneswswnese',
-#     'swwesenesewenwneswnwwneseswwne',
-#     'enesenwswwswneneswsenwnewswseenwsese',
-#     'wnwnesenesenenwwnenwsewesewsesesew',
-#     'nenewswnwewswnenesenwnesewesw',
-#     'eneswnwswnwsenenwnwnwwseeswneewsenese',
-#     'neswnwewnwnwseenwseesewsenwsweewe',
-#     'wseweeenwnesenwwwswnew',
-# ], days=100))
+test(10, part1(test_input_data))
+test(2208, part2(test_input_data, days=100))
 
 
 file_path = os.path.join(os.path.dirname(__file__), 'data/day24.txt')
